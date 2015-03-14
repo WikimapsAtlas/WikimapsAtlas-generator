b64fed66d0779c0817f37e327cc775906b085072
hugolpz <hugo.lpz@gmail.com>
2015-03-14 20:19:09 +0100

    dev: changes to test xhr queries on WM servers

--- js/wikiatlas.js
+++ js/wikiatlas.js
@@ -384,10 +384,10 @@ var path = d3.geo.path()
 injectPattern("svg"); //Pattern injection : disputed-in, disputed-out
 
 console.log("pattern()");
-var url1 = "https://rugger-demast.codio.io/output/"+target+"/administrative.topo.json",
-	url2 = "https://rugger-demast.codio.io/output/"+target+"/color.jpg",
-	//url3 = "https://rugger-demast.codio.io/output/"+target+"/color.jpg.b64",
-	url4 = "https://rugger-demast.codio.io/output/"+target+"/trans.png.b64";
+var url1 = "../output/"+target+"/administrative.topo.json",
+	url2 = "../output/"+target+"/color.jpg.b64",
+	//url3 = "https://rugger-demast.codio.io/output/"+target+"/color.jpg",
+	url4 = "../output/"+target+"/trans.png.b64";
 
  queue()
 	.defer(d3.json, url1)
