4bf990d6a8d71f16b20c92ffdba63d7b7892b839
hugolpz <hugo.lpz@gmail.com>
2015-05-02 02:24:30 +0200

    dev: improve script to choice relevant raster

--- script/raster_source.bash
+++ script/raster_source.bash
@@ -3,17 +3,31 @@ WEST=$1
 NORTH=$2
 EAST=$3
 SOUTH=$4
+# ------------------------------------------------------------------ #
+rm ./tmp/* ./tmp/.*
 mkdir -p ./tmp/
+# ------------------------------------------------------------------ #
 if [[ ${AREA_SIZE}>0.015 ]]; then 
 	gdal_translate -projwin $WEST $NORTH $EAST $SOUTH ../data/noaa/ETOPO1_Ice_g_geotiff.tif crop_xl.tmp.tif 
 fi 
-if [[ ${AREA_SIZE}<=0.015 ]]; then 
-	files=( "world1.tif" "world2.tif" ) 
+# ------------------------------------------------------------------ #
+if [[ ${AREA_SIZE}<=0.015 && ${AREA_SIZE} > 0.0015  ]]; then 
+	files=(`ls ../data/cgiar-csi_250/ | grep .tif$`) 
 	for i in "${!files[@]}"
 	do 
 		echo "Processing STRM: $file" 
-		gdal_translate -projwin $WEST $NORTH $EAST $SOUTH ../data/strm_test/${files[${i}]} ./tmp/crop.${i}.tmp.tif 
+		gdal_translate -projwin $WEST $NORTH $EAST $SOUTH ../data/cgiar-csi_250/${files[${i}]} ./tmp/crop.${i}.tmp.tif 
+	done
+	gdal_merge.py -o ./crop_xl.tmp.tif ./tmp/crop.*.tmp.tif -n -32768 
+fi
+# ------------------------------------------------------------------ #
+if [[ ${AREA_SIZE}<=0.0015 ]]; then 
+	files=(`ls ../data/cgiar-csi_90/ | grep .tif$`) 
+	for i in "${!files[@]}"
+	do 
+		echo "Processing STRM: $file" 
+		gdal_translate -projwin $WEST $NORTH $EAST $SOUTH ../data/cgiar-csi_90/${files[${i}]} ./tmp/crop.${i}.tmp.tif 
 	done 
 	gdal_merge.py -o ./crop_xl.tmp.tif ./tmp/crop.*.tmp.tif -n -32768 
 fi
-# takes empty files: ok :D
+# ------------------------------------------------------------------ #
