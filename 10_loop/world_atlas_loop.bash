56cb053e579bc99758ce8bde6328e3ee8631cb04
hugolpz <hugo.lpz@gmail.com>
2015-05-02 03:10:06 +0200

    add: world_atlas_loop.bash to map all countries.

---
+++ 10_loop/world_atlas_loop.bash
@@ -0,0 +1,10 @@
+#!/bin/bash
+# runs: bash ./10_loop/looping.bash ./10_loop/commands.txt
+file=$1
+while IFS= read -r line;
+do
+	$line
+done < "$file"
+
+#IFS: Internal Field Separator
+# while IFS=' '; read -r field1 field2 field3 f4 f5 ... # for input such:  India  67.0  37.5  99.0  05.0  
