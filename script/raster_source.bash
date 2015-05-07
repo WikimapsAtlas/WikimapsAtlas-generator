ea2bed46cd46cf55c5712360655661ba9674f097
hugolpz <hugo.lpz@gmail.com>
2015-05-07 23:45:39 +0200

    dev: minor

--- script/raster_source.bash
+++ script/raster_source.bash
@@ -4,12 +4,12 @@ NORTH=$2
 EAST=$3
 SOUTH=$4
 # ------------------------------------------------------------------ #
-rm ./tmp/*
+rm -f ./tmp/*
 mkdir -p ./tmp/
 # ------------------------------------------------------------------ #
 if [[ ${AREA_SIZE}>0.015 ]]; then 
 	echo "Source: 1km"
-	gdal_translate -projwin $WEST $NORTH $EAST $SOUTH ../data/noaa/ETOPO1_Ice_g_geotiff.tif crop_xl.tmp.tif 
+	gdal_translate -projwin $WEST $NORTH $EAST $SOUTH -eco ../data/noaa/ETOPO1_Ice_g_geotiff.tif crop_xl.tmp.tif 
 fi 
 # ------------------------------------------------------------------ #
 if [[ ${AREA_SIZE}<=0.015 && ${AREA_SIZE} > 0.0015  ]]; then 
@@ -18,7 +18,7 @@ if [[ ${AREA_SIZE}<=0.015 && ${AREA_SIZE} > 0.0015  ]]; then
 	for i in "${!files[@]}"
 	do 
 		echo "Processing STRM: $file" 
-		gdal_translate -projwin $WEST $NORTH $EAST $SOUTH ../data/cgiar-csi_250/${files[${i}]} ./tmp/crop.${i}.tmp.tif 
+		gdal_translate -projwin $WEST $NORTH $EAST $SOUTH -eco ../data/cgiar-csi_250/${files[${i}]} ./tmp/crop.${i}.tmp.tif 
 	done
 	gdal_merge.py -o ./crop_xl.tmp.tif ./tmp/crop.*.tmp.tif -n -32768 
 fi
