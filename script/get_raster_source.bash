b34f120c8d12b447fcb3b43f077dcd14b98a4444
hugolpz <hugo.lpz@gmail.com>
2015-05-19 13:41:04 +0200

    dev: move from area based to source based.

--- script/get_raster_source.bash
+++ script/get_raster_source.bash
@@ -8,12 +8,12 @@ SOUTH=$4
 rm -f ./tmp/*
 mkdir -p ./tmp/
 # ------------------------------------------------------------------ #
-if [[ ${AREA_SIZE}>0.015 ]]; then 
+if [[ ${AREA_SIZE} == "etopo" ]]; then 
 	echo "Source selected: ETOPO (1km/px)"
 	gdal_translate -projwin $WEST $NORTH $EAST $SOUTH -eco ../data/noaa/ETOPO1_Ice_g_geotiff.tif crop_xl.tmp.tif 
 fi
 # ------------------------------------------------------------------ #
-if [[ ${AREA_SIZE}<=0.015 && ${AREA_SIZE} > 0.0015  ]]; then 
+if [[ ${AREA_SIZE} == "srtm250"  ]]; then 
 	echo "Source selected: SRTM (1px=250m)"
 	files=(`ls ../data/cgiar-csi_250/ | grep .tif$`) 
 	for i in "${!files[@]}"
@@ -27,13 +27,13 @@ if [[ ${AREA_SIZE}<=0.015 && ${AREA_SIZE} > 0.0015  ]]; then
 	gdal_merge.py -o ./crop_xl.tmp.tif ./tmp/crop.*.tmp.tif		# -n -32768 
 fi
 # ------------------------------------------------------------------ #
-if [[ ${AREA_SIZE}<=0.0015 ]]; then 
+if [[ ${AREA_SIZE} == "srtm90" ]]; then 
 	echo "Source selected: SRTM(1px= 90m)"
 	files=(`ls ../data/cgiar-csi_90/ | grep .tif$`) 
 	for i in "${!files[@]}"
 	do
 		echo "Processing STRM: ${files[${i}]}" 
-		gdal_translate -projwin $WEST $NORTH $EAST $SOUTH ../data/cgiar-csi_90/${files[${i}]} ./tmp/crop.${i}.tmp.tif 
+		gdal_translate -projwin $WEST $NORTH $EAST $SOUTH -eco ../data/cgiar-csi_90/${files[${i}]} ./tmp/crop.${i}.tmp.tif 
 	done
 	echo "Merging raw tiles: -----------------------------"
 	gdal_merge.py -o ./crop_xl.tmp.tif ./tmp/crop.*.tmp.tif		# -n -32768 
@@ -42,6 +42,7 @@ fi
 # ------------------------------------------------------------------ #
 if [[ ${AREA_SIZE} == "test" ]]; then 
 	echo "Source selected: test (ETOPO 1km/px samples)"
+
 	files=(`ls ../data/test/ | grep .tif$`) 
 	for i in "${!files[@]}"
 	do 
