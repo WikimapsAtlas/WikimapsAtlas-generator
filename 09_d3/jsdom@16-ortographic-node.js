e75679fbecf013f9c65dca6493955683c3e097a3
hugolpz <hugo.lpz@gmail.com>
2020-09-04 12:54:03 +0200

    data: add carefully compressed world jsons with metadata

---
+++ 09_d3/jsdom@16-ortographic-node.js
@@ -0,0 +1,5 @@
+//  window.localisator("#hook2", 600, name, name, WEST, NORTH, EAST, SOUTH);
+//  console.log("Globe map, done: "+ new Date() );
+//  fs.writeFileSync(name_+"-orthographic_(2020).svg", svgheader + window.d3.select("#hook2").html())
+//  window.d3.select("#hook2").remove();
+// END svg design
