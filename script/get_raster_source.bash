14f92482ece301d93ed4a9469a642adb701df216
hugolpz <hugo.lpz@gmail.com>
2020-09-08 13:01:14 +0200

    minor (but may break some commands)

--- script/get_raster_source.bash
+++ script/get_raster_source.bash
@@ -5,9 +5,12 @@ WEST=$1
 NORTH=$2
 EAST=$3
 SOUTH=$4
+SOURCE="etopo" # to use in later developments
 # Context ---------------------------------------------------------- #
 rm -f ./tmp/*
 mkdir -p ./tmp/
+# AREA_SIZE should be steradian
+# SOURCE should be used for etopo, srtm250, srtm90
 # ------------------------------------------------------------------ #
 # Core ------------------------------------------------------------- #
 # ------------------------------------------------------------------ #
@@ -43,15 +46,20 @@ fi
 
 
 echo "Merging tiles: -------------------------------"
-gdal_merge.py -o ./crop_xl_raw.tmp.tif ./tmp/crop.*.tmp.tif	|| true	# -n -32768 ; <output_file> <input_file>
+if [[ ${AREA_SIZE} == "srtm250" || ${AREA_SIZE} == "srtm90" || ${AREA_SIZE} == "test" ]]; then
+	echo "Merging tiles: -------------------------------"
+	gdal_merge.py -o ./crop_xl_raw.tmp.tif ./tmp/crop.*.tmp.tif	|| true	# -n -32768 ; <output_file> <input_file>
+fi
 
 # ------------------------------------------------------------------ #
 #Etopo crop : ALWAYS. FOR NOW. MAY NEED CHANGE AND DROP.
 #if [[ ${AREA_SIZE} == "etopo" || -z ${AREA_SIZE} ]]; then
 	echo "Source selected: ETOPO (1km/px)"
 	gdal_translate -projwin $WEST $NORTH $EAST $SOUTH -eco ../data/noaa/ETOPO1_Ice_g_geotiff.tif ./tmp/crop_xl.etopo.tmp.tif
+	cp ./tmp/crop_xl.etopo.tmp.tif ./crop_xl.etopo.tmp.tif
+#	cp ./tmp/crop_xl.etopo.tmp.tif ./crop_xl_raw.tmp.tif
 #fi
 
-cp ./tmp/crop_xl.etopo.tmp.tif ./crop_xl.etopo.tmp.tif
+
 echo "Fillnodata: ----------------------------------"
 gdal_fillnodata.py -md 400 ./crop_xl_raw.tmp.tif ./crop_xl.tmp.tif # if pixels with no data, calculate their values, fill.; <input_file> <output_file>
